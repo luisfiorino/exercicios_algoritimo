246bb7c90438c00c4b7ec2a63b41d43327a1b1b4
Luis Felipe Fiorino Silva <luisfelipefiorino@hotmail.com>
2024-01-10 19:59:30 -0300

    Sequencing the solutions to the algorithm exercises.

---
+++ ex06.py
@@ -0,0 +1,5 @@
+numero = int(input('Digite um numero: '))
+antecessor = numero - 1
+sucessor = numero + 1
+
+print(f'O numero que voce digitou e {numero}, o seu antecessor e {antecessor}, e o seu sucessor e {sucessor}.')
---
+++ ex07.py
@@ -0,0 +1,5 @@
+numero = int(input('Digite um número: '))
+dobro = numero * 2
+terca_parte = numero / 3
+
+print(f'O dobro de {numero} e {dobro}, e a sua terça parte e {terca_parte}.')
