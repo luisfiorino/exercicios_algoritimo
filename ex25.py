0a9bde32d225e067f1d86fdf8b1258c9755134bd
Luis Felipe Fiorino Silva <luisfelipefiorino@hotmail.com>
2024-03-13 19:20:16 -0300

    dissecting variables

---
+++ ex25.py
@@ -0,0 +1,10 @@
+# Dissecando uma Variavel
+
+n = input('Digite algo: ')
+print('Eum numero? ', n.isnumeric())
+print('E alfabetico? ',n.isalpha())
+print('E um valor alfanumerico? ', n.isalnum())
+print('So tem espaços? ', n.isspace())
+print('Esta em letras maiusculas? ', n.isupper())
+print('Esta em minusculas? ', n.islower())
+print('Esta capitalizada? ', n.istitle())
