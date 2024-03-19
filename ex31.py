5487744c9726c03e128bcdd53483b281e1176c07
Luis Felipe Fiorino Silva <luisfelipefiorino@hotmail.com>
2024-03-19 19:46:37 -0300

    Update ex31.py

--- ex31.py
+++ ex31.py
@@ -8,7 +8,7 @@ segmento1 = float(input("Digite o comprimento do primeiro segmento de reta: "))
 segmento2 = float(input("Digite o comprimento do segundo segmento de reta: "))
 segmento3 = float(input("Digite o comprimento do terceiro segmento de reta: "))
 
-# Verificação se é possível formar um triângulo
+# Verificação se é possível formar um triângulo e fala o tipo de triângulo formado:
 if (segmento1 < segmento2 + segmento3) and (segmento2 < segmento1 + segmento3) and (segmento3 < segmento1 + segmento2):
     print("É possível formar um triângulo com esses segmentos de reta.")
     if segmento1 == segmento2 == segmento3:
